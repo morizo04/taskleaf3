a4b074c5da94e377b24b66aa11f8d7ca6849f755
morizo04 <omorikazuki99@gmail.com>
2020-08-19 17:52:52 +0900

    tasks,nameカラムに文字制限

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_083616) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
