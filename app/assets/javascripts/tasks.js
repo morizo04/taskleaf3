e826e5b823f4a36c6972db708bd53c906be8ef31
morizo04 <omorikazuki99@gmail.com>
2020-08-28 18:23:49 +0900

    JSのビューセット

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,14 @@
+// // 『8-2-1: Ajax でタスクを削除する』で記述するコード.
+// // 『8-2-3: コントローラからJavaScriptを返して実行する』で不要になるためにコメントアウトする.
+
+// document.addEventListener('turbolinks:load', function() {
+//   document.querySelectorAll('td').forEach(function(td) {
+//     td.addEventListener('mouseover', function(e) {
+//       e.currentTarget.style.backgroundColor = '#FF0000';
+//     });
+
+//     td.addEventListener('mouseout', function(e) {
+//       e.currentTarget.style.backgroundColor = '';
+//     });
+//   });
+// });
